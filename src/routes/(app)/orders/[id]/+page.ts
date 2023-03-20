d55d807d6c9f58181969c0ca5e5a1d6e3b50ef68
Matthew Blott <m4ttb@pm.me>
2023-03-20 21:10:06 +0000

    order changes

--- src/routes/(app)/orders/[id]/+page.ts
+++ src/routes/(app)/orders/[id]/+page.ts
@@ -1,12 +1,13 @@
+import type { PageLoad } from './$types'
 import { Order as data } from '$lib/data/order'
 import type { Order as schema } from '$lib/schema/order'
 import { error } from '@sveltejs/kit'
 
-export const load = async ({ params }): schema => {
+export const load = (async ({ params }): schema => {
 	const item = await data.fetchById(params.id)
 
 	if (item) {
 		return item
 	}
 	throw error(404, 'Not found')
-}
+}) satisfies PageLoad
